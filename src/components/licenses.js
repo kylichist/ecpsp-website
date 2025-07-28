785272d17472148b4a987aa3ce03ae477cf0d61f
Petr Pimenov <pimenovpetya@gmail.com>
2025-07-28 20:39:14 +0300

    Добавлены лицензии в формате PDF

--- src/components/licenses.js
+++ src/components/licenses.js
@@ -1,145 +1,40 @@
-import React, {Fragment, useEffect} from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import './licenses.css'
 
-const Licenses = (props) => {
-    useEffect(() => {
-        const modal = document.getElementById('licenses-modal')
-        const modalImg = document.getElementById('licenses-modal-img')
+const licenses = [
+    {title: 'Лицензия 1', img: '/images/licenses/1.jpg', pdf: '/pdf/licenses/1.pdf'},
+    {title: 'Лицензия 2', img: '/images/licenses/2.jpg', pdf: '/pdf/licenses/2.pdf'},
+    {title: 'Лицензия 3', img: '/images/licenses/3.jpg', pdf: '/pdf/licenses/3.pdf'},
+    {title: 'Лицензия 4', img: '/images/licenses/4.jpg', pdf: '/pdf/licenses/4.pdf'},
+    {title: 'Лицензия 5', img: '/images/licenses/5.jpg', pdf: '/pdf/licenses/5.pdf'},
+    {title: 'Лицензия 6', img: '/images/licenses/6.jpg', pdf: '/pdf/licenses/6.pdf'},
+]
 
-        const images = document.querySelectorAll('.licenses-item img')
-        images.forEach((img) => {
-            img.addEventListener('click', () => {
-                modalImg.src = img.src
-                modal.classList.add('show')
-                document.body.style.overflow = 'hidden'
-            })
-        })
-
-        if (modal) {
-            modal.addEventListener('click', (e) => {
-                if (e.target === modal) {
-                    modal.classList.remove('show')
-                    document.body.style.overflow = ''
-                }
-            })
-        }
-
-        return () => {
-            images.forEach((img) => {
-                img.replaceWith(img.cloneNode(true))
-            })
-        }
-    }, [])
-
+const Licenses = () => {
     return (
         <div id="licenses">
-            <div className={`licenses-container10 ${props.rootClassName}`}>
-                <div>
-                    <div className="licenses-container12">
-                    </div>
-                </div>
+            <div className="licenses-container10">
+                <div className="licenses-container12"/>
                 <div id="licenses-scroll" className="licenses-section">
-                    <h2 className="licenses-title">
-                        {props.heading ?? (
-                            <Fragment>
-                                <span className="licenses-text16">Лицензии</span>
-                            </Fragment>
-                        )}
-                    </h2>
+                    <h2 className="licenses-title">Лицензии</h2>
                     <div className="licenses-grid">
-                        <div className="licenses-item">
-                            <img alt={props.imageAlt} src={props.imageSrc}/>
-                            <p>
-                                {props.text1 ?? (
-                                    <Fragment>
-                    <span className="licenses-text17">
-                      Лицензия на ведение деятельности A
-                    </span>
-                                    </Fragment>
-                                )}
-                            </p>
-                        </div>
-                        <div className="licenses-item">
-                            <img alt={props.imageAlt1} src={props.imageSrc1}/>
-                            <p>
-                                {props.text2 ?? (
-                                    <Fragment>
-                    <span className="licenses-text19">
-                      Разрешение от организации B
-                    </span>
-                                    </Fragment>
-                                )}
-                            </p>
-                        </div>
-                        <div className="licenses-item">
-                            <img alt={props.imageAlt2} src={props.imageSrc2}/>
-                            <p>
-                                {props.text3 ?? (
-                                    <Fragment>
-                                        <span className="licenses-text20">Лицензия C</span>
-                                    </Fragment>
-                                )}
-                            </p>
-                        </div>
-                        <div className="licenses-item">
-                            <img alt={props.imageAlt3} src={props.imageSrc3}/>
-                            <p>
-                                {props.text4 ?? (
-                                    <Fragment>
-                                        <span className="licenses-text21">Сертификация D</span>
-                                    </Fragment>
-                                )}
-                            </p>
-                        </div>
-                    </div>
-                    <div id="licenses-modal" className="modal-licenses">
-                        <img id="licenses-modal-img" className="modal-content-licenses"/>
+                        {licenses.map((license, index) => (
+                            <a
+                                key={index}
+                                className="licenses-item"
+                                href={license.pdf}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={license.img} alt={license.title}/>
+                                <p>{license.title}</p>
+                            </a>
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
     )
-}
-
-Licenses.defaultProps = {
-    heading: undefined,
-    imageAlt: 'Лицензия 1',
-    imageAlt1: 'Лицензия 2',
-    imageAlt2: 'Лицензия 3',
-    imageAlt3: 'Лицензия 4',
-    imageSrc:
-        'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.1.0&q=80&w=1000',
-    imageSrc1:
-        'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.1.0&q=80&w=1000',
-    imageSrc2:
-        'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.1.0&q=80&w=1000',
-    imageSrc3:
-        'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.1.0&q=80&w=1000',
-    rootClassName: '',
-    text: undefined,
-    text1: undefined,
-    text2: undefined,
-    text3: undefined,
-    text4: undefined,
-}
-
-Licenses.propTypes = {
-    heading: PropTypes.element,
-    imageAlt: PropTypes.string,
-    imageAlt1: PropTypes.string,
-    imageAlt2: PropTypes.string,
-    imageAlt3: PropTypes.string,
-    imageSrc: PropTypes.string,
-    imageSrc1: PropTypes.string,
-    imageSrc2: PropTypes.string,
-    imageSrc3: PropTypes.string,
-    rootClassName: PropTypes.string,
-    text: PropTypes.element,
-    text1: PropTypes.element,
-    text2: PropTypes.element,
-    text3: PropTypes.element,
-    text4: PropTypes.element,
 }
 
 export default Licenses
