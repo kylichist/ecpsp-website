9976ec818e9470652b8cb5ced722f8f993b4099c
Petr Pimenov <pimenovpetya@gmail.com>
2025-08-26 15:43:57 +0300

    Добавлен конфиг nginx и страницы 404 и 503

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,10 @@ FROM nginx:alpine
 
 COPY --from=build /app/build /usr/share/nginx/html
 
+ARG ENVIRONMENT=prod
+
+COPY ./nginx/nginx.${ENVIRONMENT}.conf /etc/nginx/conf.d/default.conf
+
 EXPOSE 80 443
 
 CMD ["nginx", "-g", "daemon off;"]
--- src/views/not-found.js
+++ src/views/not-found.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
-import { Helmet } from 'react-helmet-async'
+import {Helmet} from 'react-helmet-async'
 
 import './not-found.css'
 
-const NotFound = (props) => {
-  return (
-    <div className="not-found-container1">
-      <Helmet>
-        <title>404 - Not Found</title>
-      </Helmet>
-      <h3>OOPS! PAGE NOT FOUND</h3>
-      <div className="not-found-container2">
-        <h1 className="not-found-text2">404</h1>
-      </div>
-      <div className="not-found-container3">
-        <h2 className="not-found-text3">
-          WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
-        </h2>
-      </div>
-    </div>
-  )
+const NotFound = () => {
+    return (
+        <div className="not-found-container1">
+            <Helmet>
+                <title>404 - Not Found</title>
+            </Helmet>
+            <h3>СТРАНИЦА НЕ НАЙДЕНА</h3>
+            <div className="not-found-container2">
+                <h1 className="not-found-text2">404</h1>
+            </div>
+            <div className="not-found-container3">
+                <h2 className="not-found-text3">
+                    НАМ ЖАЛЬ, НО ТАКОЙ СТРАНИЦЫ НЕ СУЩЕСТВУЕТ
+                </h2>
+            </div>
+        </div>
+    )
 }
 
 export default NotFound
